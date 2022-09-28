724b5a871f50143f2e14140db0583f67bc3bb608
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-09-28 09:56:06 -0300

    setup Root ViewController

---
+++ TwitterTutorial/TwitterTutorial/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  TwitterTutorial
+//
+//  Created by Grazi  Berti on 28/09/22.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
