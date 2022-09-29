becbea4dd3eb1920efca37a5e148ef3d6c5caa70
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-09-28 21:06:10 -0300

    create input container view and textfield with reusability

--- TwitterTutorial/TwitterTutorial/Controller/Authentication/LoginController.swift
+++ TwitterTutorial/TwitterTutorial/Controller/Authentication/LoginController.swift
@@ -18,6 +18,29 @@ class LoginController: UIViewController {
         return imageView
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let image = UIImage(systemName: "envelope")
+        let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let image = UIImage(systemName: "lock")
+        let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
+        return view
+    }()
+    
+    private lazy var emailTextField: UITextField = {
+        let textField = Utilities().textField(withPlaceholder: "Email")
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = Utilities().textField(withPlaceholder: "Password")
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -26,8 +49,8 @@ class LoginController: UIViewController {
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
-            return .lightContent
-      }
+        return .lightContent
+    }
     
     //MARK: - Selectiors
     
@@ -40,5 +63,13 @@ class LoginController: UIViewController {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor, paddingTop: 0)
         logoImageView.setDimensions(width: 150, height: 150)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stack.axis = .vertical
+        stack.spacing = 8
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,
+                     paddingLeft: 16, paddingRight: 16)
     }
 }
