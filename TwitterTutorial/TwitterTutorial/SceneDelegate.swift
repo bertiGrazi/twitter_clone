48916c5a118364ab696bd75661a8019cde5e5a32
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-10-06 14:22:31 -0300

    feat: root view controller setup and keeping user logged in

--- TwitterTutorial/TwitterTutorial/SceneDelegate.swift
+++ TwitterTutorial/TwitterTutorial/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
           let window = UIWindow(windowScene: windowScene)
           
           /// 3. Create a view hierarchy programmatically
-          let viewController = LoginController()
+          let viewController = MainTabController()
           let navigation = UINavigationController(rootViewController: viewController)
           
           /// 4. Set the root view controller of the window with your view controller
